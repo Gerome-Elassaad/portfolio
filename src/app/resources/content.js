46c790bff53e872186dc89b2c7575706cbc5d21d
Gerome Elassaad <repo-booster@proton.me>
2025-04-24 19:17:37 +1000

    implemented correct error handling

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -219,7 +219,7 @@ const about = {
 const blog = {
   path: "/blog",
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "Blog Helping You Create A Portfolio Like This Yourself",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
